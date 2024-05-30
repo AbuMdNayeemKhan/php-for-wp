279a9925ea73766884162841b79b8200166570c0
Abu Md. Nayeem Khan <66042919+AbuMdNayeemKhan@users.noreply.github.com>
2024-05-31 02:55:01 +0600

    Create index.php
    Start learning php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $test = "Hello World 1";
+    $another = "We Love PHP";
+    $str = "Abu Md Nayeem Khan";
+    $int = 1619981998;
+    $bull = true;
+    var_dump($str);
+    echo $another;
+?>
