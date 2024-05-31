c6e415b49e85a265d4dd6c175c469de0f022ce48
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-05-31 18:10:32 +0600

    learn about php loop

--- index.php
+++ index.php
@@ -20,22 +20,41 @@
     echo $div;
 
     // Compearijon operator
-    var_dump($x == $y);
-    var_dump($x === $y);
-    var_dump($x != $y);
-    var_dump($x !== $y);
-    var_dump($x < $y);
-    var_dump($x > $y);
-    var_dump($x <= $y);
-    var_dump($x >=  $y);
-
+    // var_dump($x == $y);
+    // var_dump($x === $y);
+    // var_dump($x != $y);
+    // var_dump($x !== $y);
+    // var_dump($x < $y);
+    // var_dump($x > $y);
+    // var_dump($x <= $y);
+    // var_dump($x >=  $y);
     $marks = 34;
+
     // conditional operator
-    if($marks <= 33 && $marks >= 100){
-        echo "Pass";
-    } else if ($marks < 0 || $marks > 100){
-        echo "In valid marks";
-    } else {
-        echo "Fail";
-    }
+    // if($marks <= 33 && $marks >= 100){
+    //     echo "Pass";
+    // } else if ($marks < 0 || $marks > 100){
+    //     echo "In valid marks";
+    // } else {
+    //     echo "Fail";
+    // }
+
+
 ?>
+    <ul>
+        <?php
+            //for each loop
+            $theBoys = ["Nayeem", "Zioun", "Tishat", "Rafi", "Rohan", "Sohan"];
+            $theGirls = [
+                "Shatul" => 20,
+                "Meghla" => 21, 
+                "Anika" => 12
+            ];
+
+            var_dump($theGirls)
+
+            // foreach($theGirls as $name => $age){
+            //     echo "<li>$name is $age years old</li>";
+            // }
+        ?>
+    </ul>
