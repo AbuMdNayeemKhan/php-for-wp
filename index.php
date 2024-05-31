6d8328539200665a723177abf3f4dd429f01d33d
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-05-31 13:19:16 +0600

    learn about conditional javascript

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
 <?php
-    $test = "Hello World 1";
-    $another = "We Love PHP";
-    $str = "Abu Md Nayeem Khan";
-    $int = 1619981998;
-    $bull = true;
-    var_dump($str);
-    echo $another;
+    // Variables
+    // $test = "Hello World 1";
+    // $another = "We Love PHP";
+    // $str = "Abu Md Nayeem Khan";
+    // $int = 1619981998;
+    // $bull = true;
+    // var_dump($str);
+    // echo $another;
+
+    // Arithmetic Operators
+    $x = 10;
+    $y = 2;
+    $add = $x + $y;
+    $sub = $x - $y;
+    $mul = $x * $y;
+    $div = $x / $y;
+    $div = $x % $y;
+    $div = $x ** $y;
+    echo $div;
+
+    // Compearijon operator
+    var_dump($x == $y);
+    var_dump($x === $y);
+    var_dump($x != $y);
+    var_dump($x !== $y);
+    var_dump($x < $y);
+    var_dump($x > $y);
+    var_dump($x <= $y);
+    var_dump($x >=  $y);
+
+    $marks = 34;
+    // conditional operator
+    if($marks <= 33 && $marks >= 100){
+        echo "Pass";
+    } else if ($marks < 0 || $marks > 100){
+        echo "In valid marks";
+    } else {
+        echo "Fail";
+    }
 ?>
